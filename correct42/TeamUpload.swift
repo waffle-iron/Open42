bc0358b1b626c8232f47959c1c2f826158e7357c
espiroux <emeric.spiroux@gmail.com>
2016-04-25 21:48:21 +0200

    implement TeamUpload, rm Class Scale for clearity and create dependencie model : Upload

--- correct42/TeamUpload.swift
+++ correct42/TeamUpload.swift
@@ -6,4 +6,37 @@
 //  Copyright © 2016 42. All rights reserved.
 //
 
-import Foundation
+class TeamUpload : SuperModel {
+	var id:Int{
+		get{ return (jsonData["id"].intValue) }
+		set{ jsonData["id"].int = newValue }
+	}
+
+	var finalMark:Int{
+		get{ return (jsonData["final_mark"].intValue) }
+		set{ jsonData["final_mark"].int = newValue }
+	}
+
+	
+	
+	var comment:String{
+		get{ return (jsonData["comment"].stringValue) }
+		set{ jsonData["comment"].string = newValue }
+	}
+
+	var createdAt:String{
+		get{ return (jsonData["created_at"].stringValue) }
+		set{ jsonData["created_at"].string = newValue }
+	}
+
+	
+	var uploadId:Int{
+		get{ return (jsonData["upload_id"].intValue) }
+		set{ jsonData["upload_id"].int = newValue }
+	}
+
+	var upload:Upload{
+		get{ return (Upload(jsonFetch: jsonData["upload"])) }
+	}
+
+}
