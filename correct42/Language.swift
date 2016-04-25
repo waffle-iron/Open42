ac0eb560614e4cbe66f7c77099dbba47595061b9
espiroux <emeric.spiroux@gmail.com>
2016-04-26 00:10:19 +0200

    implement language model

--- correct42/Language.swift
+++ correct42/Language.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2016 42. All rights reserved.
 //
 
-import Foundation
+class Language : SuperModel {
+	var id:Int{
+		get{ return (jsonData["id"].intValue) }
+		set{ jsonData["id"].int = newValue }
+	}
+	
+	var name:String{
+		get{ return (jsonData["name"].stringValue) }
+		set{ jsonData["name"].string = newValue }
+	}
+	
+	var identifier:String{
+		get{ return (jsonData["identifier"].stringValue) }
+		set{ jsonData["identifier"].string = newValue }
+	}
+
+	var createdAt:String{
+		get{ return (jsonData["created_at"].stringValue) }
+		set{ jsonData["created_at"].string = newValue }
+	}
+	
+	var updatedAt:String{
+		get{ return (jsonData["updated_at"].stringValue) }
+		set{ jsonData["updated_at"].string = newValue }
+	}
+	
+}
