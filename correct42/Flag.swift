327750f8ec8df71932cafbd3cb5b6bf6ccada46b
espiroux <emeric.spiroux@gmail.com>
2016-04-26 00:03:14 +0200

    implement Flag model

--- correct42/Flag.swift
+++ correct42/Flag.swift
@@ -6,4 +6,35 @@
 //  Copyright © 2016 42. All rights reserved.
 //
 
-import Foundation
+class Flag : SuperModel{
+	
+	var id:Int{
+		get{ return (jsonData["id"].intValue) }
+		set{ jsonData["id"].int = newValue }
+	}
+	
+	var name:String{
+		get{ return (jsonData["name"].stringValue) }
+		set{ jsonData["name"].string = newValue }
+	}
+	
+	var positive:Bool{
+		get{ return (jsonData["positive"].boolValue) }
+		set{ jsonData["positive"].bool = newValue }
+	}
+	
+	var icon:String{
+		get{ return (jsonData["icon"].stringValue) }
+		set{ jsonData["icon"].string = newValue }
+	}
+	
+	var createdAt:String{
+		get{ return (jsonData["created_at"].stringValue) }
+		set{ jsonData["created_at"].string = newValue }
+	}
+	
+	var updatedAt:String{
+		get{ return (jsonData["updated_at"].stringValue) }
+		set{ jsonData["updated_at"].string = newValue }
+	}
+}
