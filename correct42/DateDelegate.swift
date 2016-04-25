17ab8522880b7c643f435b71d27e2cb859381211
espiroux <emeric.spiroux@gmail.com>
2016-04-26 00:24:01 +0200

    implement Campus and begin protocol implementation

---
+++ correct42/DateDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  DateDelegate.swift
+//  correct42
+//
+//  Created by larry on 26/04/2016.
+//  Copyright © 2016 42. All rights reserved.
+//
+
+protocol DateDelegate {
+	var createdAt:String{
+		get
+		set
+	}
+	
+	var updatedAt:String{
+		get
+		set
+	}
+}
---
+++ correct42/IdDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  Identifiable.swift
+//  correct42
+//
+//  Created by larry on 26/04/2016.
+//  Copyright © 2016 42. All rights reserved.
+//
+
+protocol IdDelegate {
+	var id:Int {
+		get
+		set
+	}
+	
+	var name:String {
+		get
+		set
+	}
+}
